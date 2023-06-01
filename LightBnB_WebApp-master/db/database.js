13c91aae170a5132d84d859e4e64c2a40cd7705a
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-06-01 15:08:46 -0400

    refactored getAllProperties in database.js to allow for search functionality with filters

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -23,7 +23,7 @@ const getUserWithEmail = function(email) {
     SELECT * FROM users
     WHERE email = $1
     `;
-    
+
   return pool
     .query(queryString, [email])
     .then(result => result.rows[0] || null)
@@ -102,15 +102,64 @@ const getAllReservations = function(guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  LEFT JOIN property_reviews ON properties.id = property_id
+  `;
+
+  let clause;
+  const queryClause = function(array) {
+    array.length === 1 ? clause = 'WHERE' : clause = 'AND';
+  } 
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryClause(queryParams)
+    queryString += `${clause} owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryClause(queryParams)
+    queryString += `${clause} cost_per_night >= $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryClause(queryParams)
+    queryString += `${clause} cost_per_night <= $${queryParams.length} `;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryClause(queryParams)
+    queryString += `${clause} rating >= $${queryParams.length} `;
+  }
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+    .query(queryString, queryParams)
+    .then(result => result.rows)
+    .catch(error => console.log(error));
 };
 
 /**
